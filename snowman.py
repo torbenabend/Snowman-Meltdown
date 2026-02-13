4842d441a393d894a3ced527c19721c6bd91c103
Torben Abend <torben.abend@gmail.com>
2026-02-13 08:50:21 +0100

    Added features: snowman art, display of game state

--- snowman.py
+++ snowman.py
@@ -2,11 +2,50 @@ import random
 
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
-
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
 
 def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
+
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    print(STAGES[mistakes])
+    display_word = ""
+    for char in secret_word:
+        if char in guessed_letters:
+            display_word += char + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word)
 
 
 def play_game():
@@ -14,6 +53,9 @@ def play_game():
     print("Welcome to Snowman Meltdown!")
     print(
         "Secret word selected: " + secret_word)  # for testing, later remove this line
+    mistakes = 0
+    guessed_letters = []
+    display_game_state(mistakes, secret_word, guessed_letters)
 
     # TODO: Build your game loop here.
     # For now, simply prompt the user once:
